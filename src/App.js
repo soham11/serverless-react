09504f5a1748b18f8f4dd15d9f7cb38bfffc6caa
soham11 <sohamchakraborty1997@gmail.com>
2020-09-07 13:22:07 +0530

    added GraphQL Api

--- src/App.js
+++ src/App.js
@@ -1,27 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { API, graphqlOperation } from 'aws-amplify';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const listTodos = `query listTodos {
+  listTodos{
+    items{
+      id
+      name
+      description
+    }
+  }
+}`;
+
+const addTodo = `mutation createTodo($name:String! $description: String!) {
+  createTodo(input:{
+    name:$name
+    description:$description
+  }){
+    id
+    name
+    description
+  }
+}`;
+
+class App extends Component {
+  todoMutation = async () => {
+    const todoDetails = {
+      name: "Party tonight!",
+      description: "Amplify CLI rocks!"
+    };
+
+    const newTodo = await API.graphql(graphqlOperation(addTodo, todoDetails));
+    alert(JSON.stringify(newTodo));
+  };
+
+  listQuery = async () => {
+    console.log("listing todos");
+    const allTodos = await API.graphql(graphqlOperation(listTodos));
+    alert(JSON.stringify(allTodos));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <AmplifySignOut />
+        <p> Click a button </p>
+        <button onClick={this.listQuery}>GraphQL List Query</button>
+        <button onClick={this.todoMutation}>GraphQL Todo Mutation</button>
+      </div>
+    );
+  }
 }
 
 export default withAuthenticator(App, true);
